9f2843a278e71958bf663d0f2e683f63446ab08c
Maku459 <hzk.butterfly459@gmail.com>
2021-08-12 20:24:27 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <input type="text" v-model="current">
+    <button @click="clear_current()">clear</button>
+    <p>
+      {{direction}}
+    </p>
+    <input type="text" v-model="destination">
+    <button @click="clear_destination()">clear</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data () {
+    return {
+      current: '高田馬場',
+      destination: "渋谷"
+    }
+  },
+  methods: {
+    clear_current () {
+      this.current = ''
+    },
+    clear_destination () {
+      this.destination = ''
+    },
+  },
+  computed: {
+        direction: function() {
+            return "内回り";
+        }
+    }
 }
 </script>
 
